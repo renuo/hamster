7d566f44ae295e32aeb5c833083146237d1bce1a
Philip Breuer <philip.breuer@pm.me>
2020-05-28 13:43:08 +0200

    Add random image names

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,11 @@
 use hamster::ham::{HamImage};
+extern crate rand;
+
+use rand::Rng;
 
 fn main() {
-    let img = image::open("data_in/3.jpg").unwrap();
+    let mut rng = rand::thread_rng();
+    let img = image::open("data_in/1.png").unwrap();
     let h = HamImage::from_rgb(img.to_rgb());
-    h.to_rgb().save("data_out/3.png").unwrap();
+    h.to_rgb().save(format!("data_out/{}.png", rng.gen::<u32>())).unwrap();
 }
